15c6082b2ecc2ac868251d731aa30f5f7f2e627d
Jarrett Bishop <jarrettwbishop@gmail.com>
2024-02-06 15:46:03 -0500

    Modify print function to display the list on screen

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ const todoList = [
 	},
 ];
 
+let currentDisplay = "Inbox";
+
 function Todo(category, title, notes, dueDate, priority) {
 	return { category, title, notes, dueDate, priority };
 }
@@ -61,13 +63,35 @@ function createCategory(name) {
 	categoryList.push(newCategory);
 }
 
+const list = document.querySelector(".todo-list");
+
 function printTodoList(category) {
 	const categorizedList = todoList.filter(
 		(todo) => todo.category == category
 	);
 
 	console.log(categorizedList);
+
+	categorizedList.forEach((todo) => {
+		const li = document.createElement("li");
+		const input = document.createElement("input");
+		const p = document.createElement("p");
+
+		input.setAttribute("type", "checkbox");
+
+		li.classList.add("todo-item");
+		input.classList.add("checkbox");
+
+		p.textContent = todo.title;
+
+		li.appendChild(input);
+		li.appendChild(p);
+
+		list.appendChild(li);
+	});
 }
+
+printTodoList("Inbox");
 
 function isExistingCategory(name) {
 	for (const category of categoryList) {
